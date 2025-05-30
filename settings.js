5ce17e9249ffe43f35760b31606697116da79162
QuantumNim <mosotinimrod25@Outlook.com>
2025-05-30 11:04:23 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Quantumnim', // Your name
-  ownerNumber: '254716738383', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'QuantumRiffin', // Your name
+  ownerNumber: '254108482610', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
